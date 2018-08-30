9be515e8d4fbb1ed113275b54eb3c482fe45f92e
Lucas Spanhol <lucastspanhol@hotmail.com>
2018-08-30 16:24:52 -0300

    Class posicao adicionada

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace Xadrez {
     class Program {
         static void Main(string[] args) {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Pos: " + P);
+
+            Console.ReadLine();
         }
     }
 }
